2ef911a32d3d0d2c0449bdd07e7949899478fd93
Matthieu Grieger <matthieugrieger@gmail.com>
2016-03-31 18:19:04 -0700

    Implemented Playlist getters

--- playlist.go
+++ playlist.go
@@ -11,11 +11,46 @@ import "time"
 
 // Playlist stores all metadata related to a playlist of tracks.
 type Playlist struct {
-	ID        string
-	Title     string
-	Author    string
-	Submitter string
-	Service   string
-	Duration  time.Duration
-	NumTracks int
+	pID        string
+	pTitle     string
+	pAuthor    string
+	pSubmitter string
+	pService   string
+	pDuration  time.Duration
+	pNumTracks int
+}
+
+// ID returns the ID of the playlist.
+func (p *Playlist) ID() string {
+	return p.pID
+}
+
+// Title returns the title of the playlist.
+func (p *Playlist) Title() string {
+	return p.pTitle
+}
+
+// Author returns the author of the playlist.
+func (p *Playlist) Author() string {
+	return p.pAuthor
+}
+
+// Submitter returns the submitter of the playlist.
+func (p *Playlist) Submitter() string {
+	return p.pSubmitter
+}
+
+// Service returns the name of the service from which the playlist was retrieved from.
+func (p *Playlist) Service() string {
+	return p.pService
+}
+
+// Duration returns the duration of the playlist.
+func (p *Playlist) Duration() time.Duration {
+	return p.pDuration
+}
+
+// NumTracks returns the number of tracks in the playlist.
+func (p *Playlist) NumTracks() int {
+	return p.pNumTracks
 }
