864d150596ad0fbe94e029c54744d430b53647a7
Matthieu Grieger <matthieugrieger@gmail.com>
2016-04-07 13:48:34 -0700

    Implemented move command

--- commands/move.go
+++ commands/move.go
@@ -6,3 +6,52 @@
  */
 
 package commands
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/layeh/gumble/gumble"
+)
+
+// MoveCommand is a command that moves the bot from one channel to another.
+type MoveCommand struct{}
+
+// Aliases returns the current aliases for the command.
+func (c *MoveCommand) Aliases() []string {
+	return DJ.BotConfig.Aliases.Move
+}
+
+// Description returns the description for the command.
+func (c *MoveCommand) Description() string {
+	return DJ.BotConfig.Descriptions.Move
+}
+
+// IsAdminCommand returns true if the command is only for admin use, and
+// returns false otherwise.
+func (c *MoveCommand) IsAdminCommand() bool {
+	return DJ.BotConfig.Permissions.Move
+}
+
+// Execute executes the command with the given user and arguments.
+// Return value descriptions:
+//    string: A message to be returned to the user upon successful execution.
+//    bool:   Whether the message should be private or not. true = private,
+//            false = public (sent to whole channel).
+//    error:  An error message to be returned upon unsuccessful execution.
+//            If no error has occurred, pass nil instead.
+// Example return statement:
+//    return "This is a private message!", true, nil
+func (c *MoveCommand) Execute(user *gumble.User, args ...string) (string, bool, error) {
+	if len(args) == 0 {
+		return "", true, errors.New("A destination channel must be supplied to move the bot")
+	}
+	if channels := strings.Split(args[0], "/"); DJ.Client.Channels.Find(channels...) != nil {
+		DJ.Client.Self.Move(DJ.Client.Channels.Find(channels...))
+	} else {
+		return "", true, errors.New("The provided channel does not exist")
+	}
+
+	return fmt.Sprintf("You have successfully moved the bot to <b>%s</b>.", args[0]), true, nil
+}
--- commands/pkg_init.go
+++ commands/pkg_init.go
@@ -38,6 +38,7 @@ func init() {
 		new(HelpCommand),
 		new(KillCommand),
 		new(ListTracksCommand),
+		new(MoveCommand),
 		new(VersionCommand),
 	}
 }
