1bee1cfca85985bd45d4216463fcf91bd20859ec
Matthieu Grieger <matthieugrieger@gmail.com>
2016-04-19 23:50:00 -0700

    Renamed getters

--- interfaces/playlist.go
+++ interfaces/playlist.go
@@ -7,13 +7,15 @@
 
 package interfaces
 
+import "time"
+
 // Playlist is an interface of methods that must be implemented by playlists.
 type Playlist interface {
-	ID() string
-	Title() string
-	Author() string
-	Submitter() string
-	Service() string
-	Duration() string
-	NumTracks() int
+	GetID() string
+	GetTitle() string
+	GetAuthor() string
+	GetSubmitter() string
+	GetService() string
+	GetDuration() time.Duration
+	GetNumTracks() int
 }
