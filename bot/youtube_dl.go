3f3732b22bfc54cbdc1de53dea4e3c695b819f97
Matthieu Grieger <matthieugrieger@gmail.com>
2016-05-20 14:38:18 -0700

    Ditched using youtube-dl to retrieve track information, its too messy

--- bot/youtube_dl.go
+++ bot/youtube_dl.go
@@ -9,13 +9,7 @@ package bot
 
 import (
 	"errors"
-	"fmt"
 	"os/exec"
-	"sync"
-	"time"
-
-	"github.com/antonholmquist/jason"
-	"github.com/layeh/gumble/gumble"
 )
 
 // YouTubeDL is a struct that gathers all methods related to the youtube-dl
@@ -33,89 +27,4 @@ func (yt *YouTubeDL) CheckInstallation() error {
 		return errors.New("youtube-dl is not properly installed")
 	}
 	return nil
-}
-
-// GetTracks returns track objects retrieved from the provided URL.
-func (yt *YouTubeDL) GetTracks(url string, user *gumble.User) ([]*Track, error) {
-	var (
-		jsonBytes  []byte
-		err        error
-		json       *jason.Object
-		extractor  string
-		isPlaylist bool
-		tracks     []*Track
-	)
-
-	command := exec.Command("youtube-dl", "--dump-json", "--flat-playlist", url)
-	jsonBytes, err = command.Output()
-	if err != nil {
-		return nil, err
-	}
-
-	json, err = jason.NewObjectFromBytes(jsonBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	extractor, err = json.GetString("extractor")
-	if err != nil {
-		// We may have a playlist, which has a different json key; check for it.
-		// If successful, we can set isPlaylist to true.
-		extractor, err = json.GetString("ie_key")
-		if err != nil {
-			return nil, errors.New("The service you attempted to use is not supported by youtube-dl")
-		}
-		isPlaylist = true
-	}
-
-	if !yt.isWhitelisted(extractor) {
-		return nil, errors.New("The service you attempted to use is not whitelisted")
-	}
-
-	if isPlaylist {
-		// Multiple webpages must be fetched and parsed. To speed this up, separate
-		// goroutines are spawned to handle each track of the playlist.
-		var trackObjects []*jason.Object
-		var waitGroup sync.WaitGroup
-
-	} else {
-		// Only one webpage must be fetched and parsed, no goroutines are necessary.
-		track := yt.createTrack(json, user, nil)
-		tracks = append(tracks, track)
-	}
-
-	return tracks, nil
-}
-
-func (yt *YouTubeDL) isWhitelisted(extractor string) bool {
-	for _, whitelistedExtractor := range DJ.BotConfig.Services.Whitelist {
-		if extractor == whitelistedExtractor {
-			return true
-		}
-	}
-	return false
-}
-
-func (yt *YouTubeDL) createTrack(json *jason.Object, user *gumble.User, playlist *Playlist) *Track {
-	// TODO: Perform better error handling here.
-	id, _ := json.GetString("id")
-	title, _ := json.GetString("title")
-	author, _ := json.GetString("uploader")
-	submitter := user.Name
-	service, _ := json.GetString("extractor")
-	thumbnailURL, _ := json.GetString("thumbnail")
-	durationSeconds, _ := json.GetInt64("duration")
-	duration, _ := time.ParseDuration(fmt.Sprintf("%ds", durationSeconds))
-
-	return &Track{
-		ID:           id,
-		Title:        title,
-		Author:       author,
-		Submitter:    submitter,
-		Service:      service,
-		Filename:     "",
-		ThumbnailURL: thumbnailURL,
-		Duration:     duration,
-		Playlist:     playlist,
-	}
 }
