45450dde3e11d9d606e5433a1de97101961ee393
Matthieu Grieger <matthieugrieger@gmail.com>
2016-05-27 00:03:17 -0700

    Started implementing YouTube API requests

--- services/youtube.go
+++ services/youtube.go
@@ -27,14 +27,14 @@ func NewYouTubeService() *YouTube {
 		&GenericService{
 			ReadableName: "YouTube",
 			TrackRegex: []string{
-				`https?:\/\/www\.youtube\.com\/watch\?v=([\w-]+)(\&t=\d*m?\d*s?)?`,
-				`https?:\/\/youtube\.com\/watch\?v=([\w-]+)(\&t=\d*m?\d*s?)?`,
-				`https?:\/\/youtu.be\/([\w-]+)(\?t=\d*m?\d*s?)?`,
-				`https?:\/\/youtube.com\/v\/([\w-]+)(\?t=\d*m?\d*s?)?`,
-				`https?:\/\/www.youtube.com\/v\/([\w-]+)(\?t=\d*m?\d*s?)?`,
+				`https?:\/\/www.youtube.com\/watch\?v=(?P<id>[\w-]+)(?P<timestamp>\&t=\d*m?\d*s?)?`,
+				`https?:\/\/youtube.com\/watch\?v=(?P<id>[\w-]+)(?P<timestamp>\&t=\d*m?\d*s?)?`,
+				`https?:\/\/youtu.be\/(?P<id>[\w-]+)(?P<timestamp>\?t=\d*m?\d*s?)?`,
+				`https?:\/\/youtube.com\/v\/(?P<id>[\w-]+)(?P<timestamp>\?t=\d*m?\d*s?)?`,
+				`https?:\/\/www.youtube.com\/v\/(?P<id>[\w-]+)(?P<timestamp>\?t=\d*m?\d*s?)?`,
 			},
 			PlaylistRegex: []string{
-				`https?:\/\/www\.youtube\.com\/playlist\?list=([\w-]+)`,
+				`https?:\/\/www\.youtube\.com\/playlist\?list=(?P<id>[\w-]+)`,
 			},
 		},
 	}
@@ -56,5 +56,34 @@ func (yt *YouTube) CheckAPIKey() error {
 // tracks associated with the URL. An error is returned
 // if any error occurs during the API call.
 func (yt *YouTube) GetTracks(url string) ([]bot.Track, error) {
-	return nil, nil
+	var (
+		videoURL         string
+		playlistURL      string
+		playlistItemsURL string
+		id               string
+		err              error
+		resp             *http.Response
+		v                *jason.Object
+	)
+
+	videoURL = "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=%s&key=%s"
+	playlistURL = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=%s&key=%s"
+	playlistItemsURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&playlistId=%s&key=%s"
+
+	if yt.isPlaylist(url) {
+		id = yt.getID(url)
+
+		resp, err = http.Get(fmt.Sprintf(playlistURL, id, DJ.BotConfig.API.YouTube))
+		defer resp.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		v, err = jason.NewObjectFromReader(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		title, _ := v.GetString("items", "0", "snippet", "title")
+	}
 }
